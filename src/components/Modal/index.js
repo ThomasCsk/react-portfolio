c73f3477809260f5281aae8bf749594d05285a83
Thomas Ciszek <tciszek515@gmail.com>
2022-07-21 22:05:40 -0400

    modal created

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Modal = () => {
+const Modal = ({ onClose, currentPhoto }) => {
+  const {name, description, website, github, index} = currentPhoto;
+  console.log(name, description, website, github, index);
   return(
     <div>
-      
+      <div>
+        <div>
+          <h3>{name}</h3>
+          <img src={require(`../../assets/ProjectScreenshots/${index}.PNG`)} alt="current project" />
+          <p>{description}</p>
+          <div>
+            <a href={website} target='_blank' rel="noreferrer">Website</a>
+            <a href={github} target='_blank' rel="noreferrer">GitHub</a>
+          </div>
+          <button onClick={onClose} type="button">Close this modal</button>
+        </div>
+      </div>
     </div>
   )
 }
